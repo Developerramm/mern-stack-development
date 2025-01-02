41b8948a0c241d02876b1ec23defc81e977348e7
ram kumar <rr697144@gmail.com>
2025-01-02 09:38:05 +0530

    javascript basic

---
+++ javascript/basic/index.js
@@ -0,0 +1,25 @@
+console.log("Hello world");
+
+let formatted = true;
+
+if(formatted){
+    console.log("The code is easy to run")
+}
+
+let message = "Welcome to javascript";
+console.log(message);
+
+if(window.localStorage){
+    console.log("localStorage is supported");
+}
+
+let counter = 100;
+console.log(counter)
+
+
+name = "ram kumar";
+console.log(name)
+
+let age;
+console.log(age)
+// console.log(college)
