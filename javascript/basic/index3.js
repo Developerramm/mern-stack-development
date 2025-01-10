54475635cf478fc25c264ebf8206e3b89887aa4d
ram kumar <rr697144@gmail.com>
2025-01-10 10:25:52 +0530

    javascript variable

---
+++ javascript/basic/index3.js
@@ -0,0 +1,28 @@
+console.log("Ram Kumar");
+// alert("Welcome to javaScript course");
+console.log("I am an external javaScript file");
+
+// variable and value;
+
+let name = "Ram Kumar";
+let Village = "Maniyari";
+
+console.log(name,Village);
+
+let _name = "Mohit";
+let $village = "Noida";
+
+console.log(_name)
+console.log($village);
+
+let yield = 29;
+console.log(yield)
+
+// we cannot use reserved keyword in javascript for variable name
+// let break = 234;
+// console.log(break)
+
+name = "Mahi Manisha Jha";
+console.log(name);
+
+
