5f02a9b5f0392bbe9bea149011520bbf8c2df5ab
ram kumar <rr697144@gmail.com>
2025-01-10 11:31:48 +0530

    practice in javascript

---
+++ javascript/basic/index5.js
@@ -0,0 +1,47 @@
+// console.log(077)
+
+console.log(parseInt("xyz"));
+console.log(parseFloat("RamKumar"));
+console.log(isNaN("1/0"));
+console.log("1/0");
+console.log(1 / 0);
+console.log(Infinity);
+console.log(typeof Infinity);
+console.log(NaN == NaN);
+console.log(NaN === NaN);
+console.log(typeof NaN);
+if (NaN == NaN) {
+  console.log("This is true");
+} else {
+  console.log("This is not true");
+}
+
+console.log("Hello" + "World");
+
+let sum = "RamKumar" - 5;
+console.log(sum)
+
+let num1 = '5' - 5;
+console.log(num1)
+console.log('5' + 5)
+console.log(5 + '5')
+console.log(" " + " ")
+console.log(" " + 0)
+console.log(true + true)
+console.log(true - true)
+console.log(true - false)
+console.log(false - true)
+console.log(false + true)
+
+let sum1 = " " + " ";
+console.log(typeof sum1)
+console.log("ram" - "kumar")
+
+let str = "ram" - "kumar";
+console.log(typeof str)
+
+console.log(false + false)
+console.log(typeof(false + false))
+
+
+  
