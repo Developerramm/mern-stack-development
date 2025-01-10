dab6c87300089cf780ec737edeef9da64d21467b
ram kumar <rr697144@gmail.com>
2025-01-10 11:05:11 +0530

    data type in javascript

---
+++ javascript/basic/index4.js
@@ -0,0 +1,51 @@
+let myAge = 33;
+console.log(myAge, typeof myAge);
+
+// let vill@name = "Maniyari";
+// console.log(vill)
+
+let _myvillage$ = "Maniyari";
+console.log(_myvillage$.length);
+console.log(_myvillage$, typeof _myvillage$);
+
+console.log("Datatype in javaScript");
+
+let name = "ram kumar";
+let age = 23;
+let areYProgramer = true;
+let village;
+let married = null;
+
+console.log(name, typeof name);
+console.log(age, typeof age);
+console.log(areYProgramer, typeof areYProgramer);
+console.log(village, typeof village);
+console.log(married, typeof married);
+
+let bigNumber = 18943294243718740148257n;
+console.log(bigNumber, typeof bigNumber);
+
+console.log(typeof null);
+console.log(typeof 23);
+console.log(typeof "ram kumar");
+console.log(typeof true);
+
+let aNumber = "23";
+console.log(+aNumber);
+console.log(typeof aNumber);
+console.log(typeof -aNumber);
+console.log(typeof Number(aNumber));
+
+console.log(typeof String(23));
+console.log(typeof NaN);
+
+let num = "234";
+console.log(num);
+console.log(parseInt(num));
+console.log(num);
+
+console.log(parseFloat(num));
+
+console.log(typeof num);
+console.log(typeof parseFloat(num));
+console.log(typeof parseInt(num));
