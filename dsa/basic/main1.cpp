aca8d1c432c943f909a643d1fc46e8e713eb8471
ram kumar <rr697144@gmail.com>
2025-01-18 11:10:28 +0530

    taking input

---
+++ dsa/basic/main1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // taking input in cpp 
+    
+    cout << "Enter a number " << endl;
+    int n;
+    cin >> n;
+
+    cout << "You have entered : " << n << endl;
+
+    return 0;
+}
