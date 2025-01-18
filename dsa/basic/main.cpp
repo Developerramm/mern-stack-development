e6a93e284bfc5d221978de20920759d980edf21f
ram kumar <rr697144@gmail.com>
2025-01-18 11:04:33 +0530

    cpp basic setup

---
+++ dsa/basic/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Hello world" << endl;
+    cout << "Hello ram kumar maniari" << endl;
+    cout << "Namaste Bbarat here " << endl;
+    cout << 2 << endl;
+    cout << 2 + 5 << endl;
+    cout << 5 % 2 << endl;
+
+    return 0;
+}
