229342036e0e932312c02d2d0546dabc38990661
ram kumar <rr697144@gmail.com>
2025-01-22 12:47:06 +0530

    javascript math object

---
+++ javascript/basic/index13.js
@@ -0,0 +1,25 @@
+console.log("JavaScript math object");
+console.log(Math.floor(43.24));
+
+let number = 43.4999;
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
+console.log(Math.round(number));
+
+console.log(Math.PI);
+
+console.log(Math.abs(number));
+
+console.log(Math.trunc(number));
+
+console.log(Math.pow(10, 2));
+console.log(10 ** 2);
+
+console.log(Math.sqrt(8));
+
+console.log(Math.log(8));
+
+console.log(Math.random());
+
+number = Math.random() * 10;
+console.log(Math.round(number));
