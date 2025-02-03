5bccb62cc7a53e612b168e62c0f4c44301f090b6
ram kumar <rr697144@gmail.com>
2025-02-03 10:36:32 +0530

    practice

--- React/demo2/src/App.jsx
+++ React/demo2/src/App.jsx
@@ -1,29 +1,16 @@
-import React from "react";
+import React from 'react'
+
 const App = () => {
-  let person = {
-    name: "Ram Kumar",
-    program: "JavaScript",
-    isProgrammer: true,
-  };
-
-  const village = () => {
-    return "Maniyari ";
-  };
-
-  let age = 18;
-
+  let students = [ ];
+  let arr = [false,null,undefined,NaN];
   return (
-    <div className="container">
-      <h4>Dynamic Value in JSX </h4>
-      <h1>Name : {person.name} </h1>
-      <h3>Language : {person.program} </h3>
-      <h3>Is programmer : {person.isProgrammer ? "Yes" : "No"} </h3>
-      <p>Your village is : {village()} </p>
-      <h3> {age >= 18 && "You can vote here "} </h3>
-      <h3> {age > 20 && "You can marry"} </h3>
-      <h3> {age > 21 || "Wait for marry "} </h3>
-    </div>
-  );
-};
+    <div>
+      <h3> {students.length === 0 && "No students found "} </h3>
+      <h3> Number of Students are { students.length} </h3>
+      <h3> size of array is {arr.length} </h3>
 
-export default App;
+    </div>
+  )
+}
+
+export default App
