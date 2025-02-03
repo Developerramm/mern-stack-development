a8a729311048e3c7aaef5beb63f83aad7d870ed9
ram kumar <rr697144@gmail.com>
2025-02-03 09:58:22 +0530

    conditional jsx rendering

--- React/demo2/src/App.jsx
+++ React/demo2/src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-
-
 const App = () => {
   let person = {
-    name : "Ram Kumar",
-    program : "JavaScript",
-    isProgrammer : true,
-  }
+    name: "Ram Kumar",
+    program: "JavaScript",
+    isProgrammer: true,
+  };
 
-  const village = ()=>{
+  const village = () => {
     return "Maniyari ";
-  }
+  };
 
+  let age = 18;
 
   return (
     <div className="container">
@@ -19,7 +18,10 @@ const App = () => {
       <h1>Name : {person.name} </h1>
       <h3>Language : {person.program} </h3>
       <h3>Is programmer : {person.isProgrammer ? "Yes" : "No"} </h3>
-      <p>Your village is :  {village()} </p>
+      <p>Your village is : {village()} </p>
+      <h3> {age >= 18 && "You can vote here "} </h3>
+      <h3> {age > 20 && "You can marry"} </h3>
+      <h3> {age > 21 || "Wait for marry "} </h3>
     </div>
   );
 };
