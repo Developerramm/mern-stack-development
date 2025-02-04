0d0bbb634b6c3e2f68cd2c7e960137d4398339b9
ram kumar <rr697144@gmail.com>
2025-02-04 10:44:01 +0530

    event propogation in reaact part 1

--- React/demo3/src/App.jsx
+++ React/demo3/src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import EventHandling from './Components/EventHandling'
-import ParentComponent from './Components/ParentComponent'
+import React from "react";
+import EventHandling from "./Components/EventHandling";
+import ParentComponent from "./Components/ParentComponent";
+import EventPropogation from "./Components/EventPropogation";
 
 const App = () => {
   return (
-    <div className='container'>
+    <div className="container">
+      <EventPropogation />
       <EventHandling />
       <ParentComponent />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ React/demo3/src/Components/EventPropogation.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const EventPropogation = () => {
+  const handleGrandParent = () => {
+    console.log("GrandParent clicked");
+  };
+  const handleParentClick = () => {
+    console.log("Parent clicked ");
+  };
+  const handleChildClick = (event) => {
+    console.log(event);
+    console.log("child clicked");
+  };
+  return (
+    <div>
+      <h4>Event Propogation in React js </h4>
+      <div onClick={handleGrandParent}>
+        <div onClick={handleParentClick}>
+          <div onClick={handleChildClick}>child div</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventPropogation;
