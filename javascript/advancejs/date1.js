a8ccc312ea663f1a4e2bd679855fd565a7ccaaea
ram kumar <rr697144@gmail.com>
2025-02-12 10:42:29 +0530

    setTimeout and setinterval in javascript

---
+++ javascript/advancejs/date1.js
@@ -0,0 +1,17 @@
+console.log("SetTimeout and set interval func ");
+
+let count = 1;
+
+function myCallback() {
+  console.log("MY name is ram kumar ");
+  console.log(count);
+  count = count + 1;
+}
+
+setTimeout(myCallback, 2000);
+
+console.log("Hello world ");
+console.log("Maniyari");
+console.log("sitamarhi");
+
+// setInterval(myCallback,1000);
