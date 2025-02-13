4594c72a6a43d7c5f9754bd1b6394e7957b1f5ce
ram kumar <rr697144@gmail.com>
2025-02-13 20:37:33 +0530

    asynchronous code in javascript

--- javascript/advancejs/syncAsync.js
+++ javascript/advancejs/syncAsync.js
@@ -1,13 +1,27 @@
-console.log("Sync and async code ");
+// console.log("Sync and async code ");
+
+// let fun2 = () => {
+//   console.log("fun2 start and ends");
+// };
+
+// let fun1 = () => {
+//   console.log("fun1 is start");
+//   fun2();
+//   console.log("fun1 ends ");
+// };
+
+// fun1();
 
 let fun2 = () => {
-  console.log("fun2 start and ends");
+  setTimeout(() => {
+    console.log("Fun2 start and ends ");
+  }, 2000);
 };
 
 let fun1 = () => {
   console.log("fun1 is start");
   fun2();
-  console.log("fun1 ends ");
+  console.log("Fun1 is ended");
 };
 
 fun1();
