88e72f3f8a6d6058c86c2a9ee7ecb2a83e521c39
ram kumar <rr697144@gmail.com>
2025-02-13 18:35:05 +0530

    object in javascript

--- javascript/advancejs/obj1.js
+++ javascript/advancejs/obj1.js
@@ -39,3 +39,48 @@ function demo() {
 
 demo();
 console.log(num1);
+
+let obj = {
+  name: "Ram",
+  age: 20,
+  isRunner: function () {
+    console.log(this.name + " is running daily");
+  },
+};
+
+console.log(obj);
+obj.isRunner();
+
+let newObj = Object.assign({}, obj);
+
+console.log(newObj);
+newObj.age = 30;
+console.log(obj);
+console.log(newObj);
+
+console.log(product);
+let keys = Object.keys(product);
+console.log(keys);
+let values = Object.values(product);
+console.log(values);
+
+console.log(Object.entries(product));
+
+// check two object are equal or not
+
+let compareTwoObj = (objA, objB) => {
+  return JSON.stringify(objA) == JSON.stringify(objB);
+};
+
+let objA = { name: "ram", age: 30, city: "Noida" };
+let objB = { name: "ram", age: 30, city: "Noida" };
+
+let objC = { name: "Sarita", age: 24, city: "Noida" };
+console.log(objA == objB); // false
+// console.log(Object.toJSON(objA) == Object.toJSON(objB))
+// let eq = Object.toJSON(objA) == Object.toJSON(objB)
+// console.log(eq)
+
+// console.log(objA.length)
+
+console.log(JSON.stringify(objA) == JSON.stringify(objB));
