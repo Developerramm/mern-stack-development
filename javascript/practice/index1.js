d47f1de545fc7a9d3adc60f26143e998ad54f883
ram kumar <rr697144@gmail.com>
2025-02-15 16:56:27 +0530

    call back hell in javascript

---
+++ javascript/practice/index1.js
@@ -0,0 +1,18 @@
+const getStudentData = () => {
+  setTimeout(() => {
+    console.log("My name is ram kumar");
+    setTimeout(() => {
+      console.log("I am from maniyari ");
+      setTimeout(() => {
+        console.log("City is Sitamarhi ");
+        setTimeout(() => {
+          console.log("I like coding ");
+          setTimeout(() => {
+            console.log("How are you ?");
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+};
+getStudentData();
