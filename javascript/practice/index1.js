495fe27cd340e176b32774126de22a74d832c309
ram kumar <rr697144@gmail.com>
2025-02-15 17:44:51 +0530

    promise in javascript

--- javascript/practice/index1.js
+++ javascript/practice/index1.js
@@ -1,18 +1,39 @@
-const getStudentData = () => {
+// const getStudentData = () => {
+//   setTimeout(() => {
+//     console.log("My name is ram kumar");
+//     setTimeout(() => {
+//       console.log("I am from maniyari ");
+//       setTimeout(() => {
+//         console.log("City is Sitamarhi ");
+//         setTimeout(() => {
+//           console.log("I like coding ");
+//           setTimeout(() => {
+//             console.log("How are you ?");
+//           }, 1000);
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// };
+// getStudentData();
+
+const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    console.log("My name is ram kumar");
-    setTimeout(() => {
-      console.log("I am from maniyari ");
-      setTimeout(() => {
-        console.log("City is Sitamarhi ");
-        setTimeout(() => {
-          console.log("I like coding ");
-          setTimeout(() => {
-            console.log("How are you ?");
-          }, 1000);
-        }, 1000);
-      }, 1000);
-    }, 1000);
-  }, 1000);
-};
-getStudentData();
+    resolve("Hi, I miss you ");
+  }, 2000);
+});
+
+promise
+  .then((result) => {
+    console.log(result);
+  })
+
+  .catch((error) => {
+    console.log(error);
+  })
+
+  .finally(() => {
+    console.log("code is running fine here");
+  });
+
+console.log(promise);
